adfb9779fa7bbb3d1a3751bb56a610d602ba12d0
Theresa <tbarton16@gmail.com>
2014-07-23 17:05:19 -0500

    added in linear regression machinery, extracts points from correct bands, still need to feed into machinery...

--- radcal.cpp
+++ radcal.cpp
@@ -23,12 +23,16 @@ b*e used as points of no change for radiometric normalization. Using the (x,y
 *   INCLUDE FILES
 ***************************************************************************/
 // to compile g++ -I /usr/local/Cellar/eigen/3.2.1/include/eigen3 radcal.cpp GdalFileIO.cpp -lgdal
+
 #include "gdal_priv.h"
 #include "cpl_conv.h" // for CPLMalloc()
 #include "stdio.h"
 #include "time.h"
 #include "radcal.h"
 #include "tgmath.h"
+// header files from Donovan Parks
+#include "Precompiled.hpp"
+#include "LinearRegression.hpp"
 
 #include <ogr_spatialref.h>
 #include <string>
@@ -37,80 +41,178 @@ b*e used as points of no change for radiometric normalization. Using the (x,y
 #include <Eigen/Dense>
 
 
+#include <cmath>
+#include <algorithm>
+
+/**************************************************************************
+*   NameSpaceIng
+***************************************************************************/
+
 using namespace std;
 using Eigen::MatrixXf;//this represents a matrix of arbitrary size (hence the X in MatrixXd), in which every entry is a doubl
 using Eigen::VectorXf;
 
-vector<pair<int,int> > * xygen(int n, double * padfTransform, int rasterx, int rastery){
 
-   vector<pair<int,int> > * points = new vector<pair<int,int> >();
-   double xwest, xeast, ynorth, ysouth;
-   xwest  = padfTransform[0] + 0*padfTransform[1] + 0*padfTransform[2];
-   xeast = padfTransform[0] + rasterx *padfTransform[1] + 0*padfTransform[2];
-   ynorth = padfTransform[3] + 0*padfTransform[4] + 0*padfTransform[5];
-   ysouth = padfTransform[3] + 0*padfTransform[4] + rastery*padfTransform[5];
+/**************************************************************************
+*   Data points input
+***************************************************************************/
+vector<pair<int,int> > * xyget(int n){
 
-   for(int i =0; i< n; i++){
-      int  first, second;
-      first = static_cast<int>(xwest + pow(10,i));
-      second = static_cast<int>(ynorth +pow(10,i));
-      points->push_back(make_pair(first,second));
-      //cout << points-> at(i).first << ", " << points-> at(i).second << '\n';
-   }
-   return points;
+   vector<pair<int,int> > * generatedpoints = new vector<pair<int,int> >();
+   generatedpoints->push_back(make_pair(3637,13794));
+   generatedpoints->push_back(make_pair(9733	,11170	));
+   generatedpoints->push_back(make_pair(10946, 3574));
 
-}
-
-
-void radcal(){
-
-   GDALAllRegister();
-   OGRSpatialReference oSRS;
-   // inputs
-   string filename="tjpeg.tif";
-
-
-   //extract bands at (x,y)
-   GDALDataset* file1 = GdalFileIO::openFile(filename);
-
-   double * transform = new double[6];
-   file1->GetGeoTransform(transform);
-   //cout << transform[0] << '\n' << transform[3]<< '\n'<< transform[1] << '\n' << transform[5]<< '\n';
-
-   vector<pair<int,int> > * generatedpoints = xygen(10,transform,file1->GetRasterXSize(),file1->GetRasterYSize());
-   int numpoints = generatedpoints->size();
-
-//extract points
-   int numbands = file1->GetRasterCount();
-   // put in own func?
-   MatrixXf pixVals(numpoints,numbands);
-
-   for( int i =1;i< numbands;i++){
-      GDALRasterBand *band;
-      band = file1->GetRasterBand(i);
-      for(int j = 0;j<numpoints; j++){
-         int pixel= generatedpoints-> at(j).first;
-         int line = generatedpoints-> at(j).second;
-         float val;
-         float *buffer;
-         file1->RasterIO(GF_Read,pixel,line,0,0,buffer, 1, 1,GDT_Float32,1,NULL, 0, 0,0);
-         cout << *buffer;
-
-
-
-      }
-   }
-
-   // Band->RasterIO(hband, GF_Read, ;
-
-
-   //for each point read in bands
-   delete[] transform;
+   generatedpoints->push_back(make_pair(13862,12724));
+   generatedpoints->push_back(make_pair(6520,10070));
+   generatedpoints->push_back(make_pair(16168, 13875));
+   generatedpoints->push_back(make_pair(8449, 5886));
+   generatedpoints->push_back(make_pair(479, 6309));
+   generatedpoints->push_back(make_pair(1213,5256));
+   generatedpoints->push_back(make_pair(10283,1856));
+   return generatedpoints;
 
 }
 
-int main()
+/**************************************************************************
+*   Linear Regression Code (from Donovan Parks)
+***************************************************************************/
+
+bool LinearRegression::LeastSquaresEstimate(std::vector<double>& x,
+std::vector<double>& y, RESULTS& results)
 {
-   radcal();
-   return 0;
-}
+   int numPts = x.size();
+   if(numPts < 2)
+      return false;
+
+      double sumX = 0;
+      double sumY = 0;
+      double sumXY = 0;
+      double sumX2 = 0;
+      double sumY2 = 0;
+      for(int i=0; i < numPts; ++i)
+         {
+            sumX += x.at(i);
+            sumY += y.at(i);
+            sumXY	+= x.at(i) * y.at(i);
+            sumX2	+= x.at(i) * x.at(i);
+            sumY2	+= y.at(i) * y.at(i);
+         }
+
+         double SSxy = sumXY - (sumX*sumY) / numPts;
+         double SSxx = sumX2 - (sumX*sumX) / numPts;
+         double SSyy = sumY2 - (sumY*sumY) / numPts;
+
+         if(SSxx != 0 && SSyy != 0)
+            {
+               results.slope = SSxy / SSxx;
+
+               results.cod = (SSxy*SSxy) / (SSxx*SSyy);
+               results.r = SSxy / sqrt(SSxx*SSyy);
+            }
+            else if(SSxx == 0 && SSyy == 0)
+            {
+               results.slope = DBL_MAX;
+               results.cod = DBL_MAX;
+               results.r = 1;
+            }
+         else
+            {
+               results.slope = DBL_MAX;
+
+               results.cod = 1;
+               results.r = -1;
+            }
+
+            results.offset = (sumY - results.slope*sumX) / numPts;
+            results.dof = numPts-2;
+            results.nPts = numPts;
+
+            double SSE = SSyy-results.slope*SSxy;
+            results.sd = sqrt(SSE / results.dof);
+            results.seos = results.sd / sqrt(SSxx);
+            results.seoi = results.sd*sqrt((1/numPts) + ((sumX/numPts)*(sumX/numPts))/SSxx);
+
+            results.tStat = results.slope / results.seos;
+
+            return true;
+         }
+
+         /**
+         * Calculate the error of a given best fit line.
+         */
+         double LinearRegression::Error(int numPts, double sumX, double sumY, double sumXY,
+         double sumX2, double sumY2)
+         {
+            double SSxx = sumX2 - (sumX*sumX) / numPts;
+            double SSyy = sumY2 - (sumY*sumY) / numPts;
+            double SSxy = sumXY - (sumX*sumY) / numPts;
+
+            double slope = SSxy / SSxx;
+            double offset = (sumY - slope*sumX) / numPts;
+
+            return SSyy - slope*SSxy;
+         }
+
+         /**************************************************************************
+         *   Radcal
+         ***************************************************************************/
+         void radcal(){
+
+            GDALAllRegister();
+            OGRSpatialReference oSRS;
+            // inputs
+            string filename="tjpeg.tif";
+            string filenameagain = "tjpeg.tif";
+            //extract bands at (x,y)
+            GDALDataset* file1 = GdalFileIO::openFile(filename);
+            GDALDataset* file2 = GdalFileIO::openFile(filenameagain);
+
+            vector<pair<int,int> > *generatedpoints = xyget(10);
+
+            int   nXSize = file1->GetRasterXSize();
+            int   nYSize = file1-> GetRasterYSize();
+            int numpoints = sizeof(generatedpoints);
+            int numbands = file1->GetRasterCount();
+            MatrixXf pixValsMat(numbands,numpoints);
+            GDALRasterBand *rasterband;
+            int nBlockXSize, nBlockYSize;
+
+            for( int band=1;band<numbands+1;band++){
+               //cout << "_____" << "band" << i << "____"<<"\n";
+               for(int point = 0;point<numpoints; point++){
+                  rasterband = file1->GetRasterBand(band);
+                  int pixel= generatedpoints-> at(point).first;
+                  int line = generatedpoints-> at(point).second;
+                  int nXSize = rasterband->GetXSize();
+                  float * value;
+                  value = (float *) CPLMalloc(sizeof(float)*nXSize);
+                  rasterband->RasterIO( GF_Read, pixel, line, 1, 1,value, 1 /*nXSize*/, 1, GDT_Float32,
+                  0, 0 );
+                  pixValsMat(band-1, point)=*value;
+                  //cout << *value<< "\n";
+
+                  CPLFree(value);
+               }
+            }
+            for(int x=0;x<numbands;x++)  // loop 3 times for three lines
+               {
+                  for(int y=0;y<numpoints;y++)  // loop for the three elements on the line
+                     {
+                        cout<<pixValsMat(x,y)<<" ";  // display the current element out of the array
+                     }
+                     cout<<endl;  // when the inner loop is done, go to a new line
+                  }
+
+            /*run Regression*/
+             for(int band = 1; band < numbands+1; band++){
+             pixValsMat(band);
+          }
+               }
+
+               int main()
+               {
+
+                  radcal();
+                  return 0;
+               }
